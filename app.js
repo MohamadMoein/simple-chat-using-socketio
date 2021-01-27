311575739a3b9733843e60974b3ac5acdb809bcd
Mohamad Moein <mohamadmoein.h@gmail.com>
2021-01-27 14:40:37 +0330

    Project is completed (:

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => console.log(`🙄 server on port ${PORT}`));
+
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+let socketsConnected = new Set();
+
+io.on('connection', onConneted);
+
+function onConneted(socket) {
+  console.log(socket.id);
+  socketsConnected.add(socket.id);
+
+  io.emit('clients-total', socketsConnected.size);
+
+  socket.on('disconnect', () => {
+    socketsConnected.delete(socket.id);
+    io.emit('clients-total', socketsConnected.size);
+  });
+
+  socket.on('message', (data) => {
+    socket.broadcast.emit('chat-message', data);
+  });
+
+  socket.on('feedback', (data) => {
+    socket.broadcast.emit('feedback', data);
+  });
+}
